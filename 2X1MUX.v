03af09b5ed93d18f941d902597d3c1325c3b8972
pranav <76247369+pranav-iitr@users.noreply.github.com>
2021-07-03 18:11:58 +0530

    Bikramjeet

--- 2X1MUX.v
+++ 2X1MUX.v
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 
-//2X1 MUX to create 16 bit register reset
 module MUX2X1(out,i0,i1,s0);
 
 output out;
-input i0,i1;
+
 input s0;
+input i0,i1;
 
 wire c1,c2;
 assign c1=(~s0)&&i0;
