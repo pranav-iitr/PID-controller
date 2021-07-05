bd32bfaa37e24f172cc0b6177272e114302d275a
pranav <76247369+pranav-iitr@users.noreply.github.com>
2021-07-05 13:34:36 +0530

    Pranav, Prince, Bikramjeet, Dev Raj, Devesh

--- pid.v
+++ pid.v
@@ -16,14 +16,15 @@ wire [15:0] previous_input;
 wire cout1;
 wire cout2;
 wire cout3;
+  
 wire cout4;
 wire [15:0] multiply1,multiply2,multiply3;
 wire [15:0] tadd2,tadd3; 
 complement comp1(previous_input,complement1);
 adder_16bit add1(16'b0000000011001000,complement1,1'b0,current_error,cout1);  
-multiplier r(current_error,16'b0000000000000011,multiply1); //kp+kd
-multiplier s(previous_error,16'b1111111111111110,multiply2);  //kd
-multiplier t(e_prev_2,16'b0000000000000010,multiply3);  //ki
+  multiplier r(current_error,16'b0000000000000011,multiply1);  
+  multiplier s(previous_error,16'b1111111111111110,multiply2); 
+  multiplier t(e_prev_2,16'b0000000000000010,multiply3);  
 adder_16bit add2(multiply1,multiply2,1'b0,tadd2,cout2);
 adder_16bit add3(tadd2,multiply3,1'b0,tadd3,cout3);
 adder_16bit add4(u_prev,tadd3,1'b0,out_temp,cout4);
